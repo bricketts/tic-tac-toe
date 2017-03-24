990035ee6c44fb511ebce25f7f3bac0269eb57b8
bricketts <brody.ricketts@gmail.com>
2017-03-24 13:34:05 -0600

    Created Enum for Free, O and X

--- src/TicTacToe/MainWindow.xaml.cs
+++ src/TicTacToe/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TicTacToe
 {
@@ -20,9 +9,30 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/TicTacToe/MarkType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// The Type of value a cell in the game currently has
+    /// </summary>
+    public enum MarkType
+    {
+        // Cell is Free
+        Free,
+        // Cell has O
+        Nought,
+        // Cell has X
+        Cross
+
+    }
+}
