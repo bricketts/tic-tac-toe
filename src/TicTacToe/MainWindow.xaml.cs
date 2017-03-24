30a47680dfb17c29e3b6edd168960107d6767365
bricketts <brody.ricketts@gmail.com>
2017-03-24 13:51:45 -0600

    Create NewGame when a new game is started. Set all values to default

--- src/TicTacToe/MainWindow.xaml.cs
+++ src/TicTacToe/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace TicTacToe
 {
@@ -45,7 +47,27 @@ namespace TicTacToe
 
         private void NewGame()
         {
-            throw new NotImplementedException();
+            // Create new blank array of free cells
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = MarkType.Free;
+            }
+
+            mPlayer1Turn = true;
+
+            // Iterate every button on the grid...
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // Change background, foreground and content to default values
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            // Make sure the game hasn't finished
+            mGameOver = false;
         }
     }
 }
