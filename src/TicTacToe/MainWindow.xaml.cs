93bc92ec5fc3c4b4028eb44ff216ce7f26563099
bricketts <brody.ricketts@gmail.com>
2017-03-24 14:22:14 -0600

    Add button functionality and check for horizontal winner on first row

--- src/TicTacToe/MainWindow.xaml.cs
+++ src/TicTacToe/MainWindow.xaml.cs
@@ -45,6 +45,9 @@ namespace TicTacToe
 
         #endregion
 
+        /// <summary>
+        /// Start new game and clears all values and sets them to default.
+        /// </summary>
         private void NewGame()
         {
             // Create new blank array of free cells
@@ -68,6 +71,66 @@ namespace TicTacToe
 
             // Make sure the game hasn't finished
             mGameOver = false;
+        }
+
+        /// <summary>
+        /// Handles a button click event
+        /// </summary>
+        /// <param name="sender">The button was clicked</param>
+        /// <param name="e">The events of the click</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (mGameOver)
+            {
+                NewGame();
+                return;
+            }
+
+            // Cast the sender to a button
+            var button = (Button)sender;
+
+            // finds the buttons position in the array
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var index = column + (row * 3);
+
+            // Dont do anything if the cell already has a value in it
+            if (mResults[index] != MarkType.Free)
+            {
+                return;
+            }
+
+            // Set the cell value based on which players turn it is
+            mResults[index] = mPlayer1Turn ? MarkType.Cross : MarkType.Nought;
+
+            // Set button text to the result
+            button.Content = mPlayer1Turn ? "X" : "O";
+
+            // Change O color to green
+            if (!mPlayer1Turn)
+                button.Foreground = Brushes.Red;
+            
+            // Toggle the players turns
+            mPlayer1Turn ^= true;
+
+            // Check for Winner
+            CheckForWinner();
+        }
+
+        private void CheckForWinner()
+        {
+            //Check for Horizontal Win
+            var same = (mResults[0] & mResults[1] & mResults[2]) == mResults[0];
+
+            if (mResults[0] != MarkType.Free && same)
+            {
+                // Game Ends
+                mGameOver = true;
+
+                // Hightlight Winning cells in green
+                Button0_0.Background = Button1_0 = Button2_0 = Brushes.Green;
+            }
         }
     }
 }
