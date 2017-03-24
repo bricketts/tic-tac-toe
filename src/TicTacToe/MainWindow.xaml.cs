243691d8fc7cc0aea92c49263f3a66379c250025
bricketts <brody.ricketts@gmail.com>
2017-03-24 14:45:44 -0600

    Implement  Vertical Win Conditions

--- src/TicTacToe/MainWindow.xaml.cs
+++ src/TicTacToe/MainWindow.xaml.cs
@@ -151,6 +151,36 @@ namespace TicTacToe
                 Button0_2.Background = Button1_2.Background = Button2_2 .Background = Brushes.Green;
             }
 
+            //Check for Vertical Win
+            //Check for Left Column Win
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+            {
+                // Game Ends
+                mGameOver = true;
+
+                // Hightlight Winning cells in green
+                Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Green;
+            }
+
+            //Check for Center Column Win
+            if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+            {
+                // Game Ends
+                mGameOver = true;
+
+                // Hightlight Winning cells in green
+                Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Green;
+            }
+
+            //Check for Right Column Win
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+            {
+                // Game Ends
+                mGameOver = true;
+
+                // Hightlight Winning cells in green
+                Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Green;
+            }
 
             // Check for no winner and default board
             if (!mResults.Any(cell => cell == MarkType.Free))
